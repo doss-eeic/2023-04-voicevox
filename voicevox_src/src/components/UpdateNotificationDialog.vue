2810637c50b94ab1bcf816539c9fd3bd23769b69
liszt01 <himuroland@gmail.com>
2023-10-24 15:12:15 +0900

    ダイアログの表示を修正

--- voicevox_src/src/components/UpdateNotificationDialog.vue
+++ voicevox_src/src/components/UpdateNotificationDialog.vue
@@ -1,46 +1,59 @@
 <template>
-    <q-dialog v-model="modelValueComputed">
-      <q-card>
-        <q-card-section class="text-h6">
-          新しいアップデートがあります！
-        </q-card-section>
-        <q-card-section>
-          <!-- href の前にコロンつけないとダメ -->
-          <q-btn
-            color="primary"
-            :href="downloadLink"
-            tag="a"
-            target="_blank"
-            label="ダウンロードページへ"
-            @click="toDialogClosedState"
-          />
-          <q-btn label="後で" @click="toDialogClosedState" />
-        </q-card-section>
-      </q-card>
-    </q-dialog>
-  </template>
-  
-  <script setup lang="ts">
-  import { computed } from "vue";
-  
-  const props =
-    defineProps<{
-      modelValue: boolean;
-    }>();
-  const emit =
-    defineEmits<{
-      (e: "update:modelValue", val: boolean): void;
-    }>();
-  
-  const modelValueComputed = computed({
-    get: () => props.modelValue,
-    set: (val) => emit("update:modelValue", val),
-  });
-  
-  const toDialogClosedState = () => {
-    modelValueComputed.value = false;
-  };
-  
-  const downloadLink = "https://voicevox.hiroshiba.jp/";
-  </script>
-  
+  <q-dialog v-if="!isUpdateAvailable" v-model="showDialog">
+    <q-card class="q-py-sm q-px-md">
+      <q-card-section align="center">
+        <div class="text-h6">
+          <q-icon name="info" color="primary" />アップデート通知
+        </div>
+        <p>
+          新しいバージョンが利用可能です。<br />
+          公式サイトから最新版をダウンロードできます。
+        </p>
+      </q-card-section>
+      <q-card-actions align="center">
+        <q-btn
+          padding="xs md"
+          label="キャンセル"
+          unelevated
+          color="surface"
+          text-color="display"
+          class="q-mt-sm"
+          @click="closeDialog()"
+        />
+        <q-btn
+          padding="xs md"
+          label="公式サイトを開く"
+          unelevated
+          color="primary"
+          text-color="display-on-primary"
+          class="q-mt-sm"
+          @click="
+            openOfficialWebsite();
+            closeDialog();
+          "
+        />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from "vue";
+import { useFetchLatestVersion } from "@/composables/useFetchLatestVersion";
+
+const { isCheckingFinished, latestVersion } = useFetchLatestVersion();
+
+const isUpdateAvailable = computed(() => {
+  return isCheckingFinished.value && latestVersion.value !== "";
+});
+
+const showDialog = ref<boolean>(true);
+
+const closeDialog = () => {
+  showDialog.value = false;
+};
+
+const openOfficialWebsite = () => {
+  window.open("https://voicevox.hiroshiba.jp/", "_blank");
+};
+</script>
