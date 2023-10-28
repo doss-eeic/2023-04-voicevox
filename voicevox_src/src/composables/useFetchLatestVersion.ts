47c69480ea440f18180f457cd175bf8a8ea921a4
Yuriko Yamauchi <uitolp-tge999-hykook@g.ecc.u-tokyo.ac.jp>
2023-10-28 17:37:12 +0900

    useFetchLatestVersion.ts の関数を統合

--- voicevox_src/src/composables/useFetchLatestVersion.ts
+++ voicevox_src/src/composables/useFetchLatestVersion.ts
@@ -1,12 +1,61 @@
 import { ref } from "vue";
 import semver from "semver";
 import { UpdateInfo as UpdateInfoObject } from "@/type/preload";
+// import { late } from "zod";
 
 // 最新版があるか調べる
-export const useFetchLatestVersion = () => {
+// export const useFetchLatestVersion = () => {
+//   const isCheckingFinished = ref<boolean>(false);
+//   const currentVersion = ref("");
+//   const latestVersion = ref("");
+
+//   window.electron
+//     .getAppInfos()
+//     .then((obj) => {
+//       currentVersion.value = obj.version;
+//     })
+//     .then(() => {
+//       fetch("https://api.github.com/repos/VOICEVOX/voicevox/releases", {
+//         method: "GET",
+//         headers: {
+//           "Content-Type": "application/json",
+//           Accept: "application/json",
+//         },
+//       })
+//         .then((response) => {
+//           if (!response.ok) throw new Error("Network response was not ok.");
+//           return response.json();
+//         })
+//         .then((json) => {
+//           const newerVersion = json.find(
+//             (item: { prerelease: boolean; tag_name: string }) => {
+//               return (
+//                 !item.prerelease &&
+//                 semver.valid(currentVersion.value) &&
+//                 semver.valid(item.tag_name) &&
+//                 semver.lt(currentVersion.value, item.tag_name)
+//               );
+//             }
+//           );
+//           if (newerVersion) {
+//             latestVersion.value = newerVersion.tag_name;
+//           }
+//           isCheckingFinished.value = true;
+//         })
+//         .catch((err) => {
+//           throw new Error(err);
+//         });
+//     });
+
+//   return { isCheckingFinished, currentVersion, latestVersion };
+// };
+
+// 最新の更新情報を取得する
+export const useFetchLatestUpdateInfos = () => {
   const isCheckingFinished = ref<boolean>(false);
   const currentVersion = ref("");
-  const latestVersion = ref("");
+  const newerVersion = ref("");
+  const latestUpdateInfos = ref<UpdateInfoObject[]>();
 
   window.electron
     .getAppInfos()
@@ -14,30 +63,20 @@ export const useFetchLatestVersion = () => {
       currentVersion.value = obj.version;
     })
     .then(() => {
-      fetch("https://api.github.com/repos/VOICEVOX/voicevox/releases", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-      })
+      fetch(
+        "https://raw.githubusercontent.com/VOICEVOX/voicevox_blog/master/src/data/updateInfos.json"
+      )
         .then((response) => {
           if (!response.ok) throw new Error("Network response was not ok.");
           return response.json();
         })
         .then((json) => {
-          const newerVersion = json.find(
-            (item: { prerelease: boolean; tag_name: string }) => {
-              return (
-                !item.prerelease &&
-                semver.valid(currentVersion.value) &&
-                semver.valid(item.tag_name) &&
-                semver.lt(currentVersion.value, item.tag_name)
-              );
-            }
-          );
-          if (newerVersion) {
-            latestVersion.value = newerVersion.tag_name;
+          latestUpdateInfos.value = json;
+          if (latestUpdateInfos.value == undefined)
+            throw new Error("Failed to fetch updateInfos.json");
+          const latestVersion = latestUpdateInfos.value[0].version;
+          if (semver.lt(currentVersion.value, latestVersion)) {
+            newerVersion.value = latestVersion;
           }
           isCheckingFinished.value = true;
         })
@@ -46,26 +85,10 @@ export const useFetchLatestVersion = () => {
         });
     });
 
-  return { isCheckingFinished, currentVersion, latestVersion };
-};
-
-// 最新の更新情報を取得する
-export const useFetchLatestUpdateInfos = () => {
-  const latestUpdateInfos = ref<UpdateInfoObject[]>();
-
-  fetch(
-    "https://raw.githubusercontent.com/VOICEVOX/voicevox_blog/master/src/data/updateInfos.json"
-  )
-    .then((response) => {
-      if (!response.ok) throw new Error("Network response was not ok.");
-      return response.json();
-    })
-    .then((json) => {
-      latestUpdateInfos.value = json;
-    })
-    .catch((err) => {
-      throw new Error(err);
-    });
-
-  return latestUpdateInfos;
+  return {
+    isCheckingFinished,
+    currentVersion,
+    newerVersion,
+    latestUpdateInfos,
+  };
 };
